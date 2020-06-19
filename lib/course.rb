b145c42140e039792e954a2a26d7eb20b8fa68e2
justinmcla <jwmclawhorn@gmail.com>
2020-06-18 20:22:15 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,28 @@
 
 
+class Course
+
+    attr_accessor :title, :schedule, :description
+
+    @@all = []
+
+    def initialize
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        self.all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+
+
+
+end
