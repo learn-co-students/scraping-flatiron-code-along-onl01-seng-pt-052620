9982850529a75934bc7f85e3ee8f4d6dacae18ae
rumikoacopa <rumikorios@gmail.com>
2020-07-02 19:33:49 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,6 +41,17 @@ class Scraper
     end
   end
 
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
   #doc.css(".post") for the collection of courses
   #doc.css(".post").first.css("h2").text for the title of an indiv. course
   #doc.css(".post").first.css(".date").text for schedule of course
